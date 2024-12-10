f3c56435d51f0c66b144c564f3ab77fedac48a24
7ipolito <allanhipolito66@gmail.com>
2024-12-10 10:52:24 -0300

    fix: to fix dashboard error after logged

--- frontend/src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ frontend/src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center w-full h-screen bg-primary">
+      <SignIn />;
+    </div>
+  );
 }
