0d23e218815c97e1af9aee16505579c8a6736c72
7ipolito <allanhipolito66@gmail.com>
2024-12-14 18:41:55 -0300

    fix: to bring userData based on client session, to create a hook to handle this

--- frontend/src/app/(root)/page.tsx
+++ frontend/src/app/(root)/page.tsx
@@ -1,73 +1,33 @@
+"use client";
 import NavBar from "@/components/Navbar";
 import ProfileCard from "@/components/ProfileCard";
 import Sponsored from "@/components/Sponsored";
-import { GET_WHOAMI } from "@/graphql/queries";
-import getToken from "@/lib/actions/token.action";
-import { getUserData } from "@/lib/actions/user.action";
-import client from "@/lib/client";
-import { UserButton } from "@clerk/nextjs";
-import {
-  Navbar,
-  NavbarBrand,
-  NavbarContent,
-  NavbarItem,
-  Button,
-  Card,
-  CardFooter,
-  Image,
-  CardHeader,
-} from "@nextui-org/react";
-import Link from "next/link";
+import { useGetUser } from "@/hooks/useGetUser";
 
-export default async function Dashboard() {
-  try {
-    const user = await getUserData();
-    console.log(user);
-    return (
-      <div className=" flex flex-1 w-full">
-        <div className="lg:min-h-screen flex flex-col  lg:max-w-screen-xl items-center justify-between mx-auto pb-32  bg-primary">
-          <NavBar />
-          <div className=" lg:grid  grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
-            <div className="">
-              <div className="flex flex-col items-center p-8 ">
-                <ProfileCard
-                  clerkUserId={user.clerkId}
-                  email={user.email}
-                  image={user.image}
-                  username={user.username}
-                  createAt={user.createAt}
-                />
-                <Sponsored />
-              </div>
+export default function Dashboard() {
+  const { data } = useGetUser();
+
+  return (
+    <div className=" flex flex-1 w-full">
+      <div className="lg:min-h-screen flex flex-col  lg:max-w-screen-xl items-center justify-between mx-auto pb-32  bg-primary">
+        <NavBar />
+        <div className=" lg:grid  grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+          <div className="">
+            <div className="flex flex-col items-center p-8 ">
+              <ProfileCard
+                clerkUserId={data?.clerkId}
+                email={data?.email}
+                image={data?.image}
+                username={data?.username}
+                createAt={data?.createAt}
+              />
+              <Sponsored />
             </div>
-            <div className="w-[500px] h-[500px]  flex justify-center items-center"></div>
-            <div className="w-[500px] h-[500px] flex justify-center items-center"></div>
           </div>
+          <div className="w-[500px] h-[500px]  flex justify-center items-center"></div>
+          <div className="w-[500px] h-[500px] flex justify-center items-center"></div>
         </div>
-
-        {/* <UserButton />
-        <h1 className="text-white">Welcome to social media graphql</h1>
-        {user ? (
-          <p className="text-white">Seu email é: {user.email}</p>
-        ) : (
-          <p className="text-red-500">
-            Erro: Dados de usuário não encontrados. Complete seu cadastro!
-          </p>
-        )} */}
       </div>
-    );
-  } catch (error) {
-    console.error("Erro ao buscar os dados do usuário:", error);
-
-    return (
-      <div className="flex flex-col flex-1 items-center w-full h-screen bg-primary">
-        {/* <UserButton />
-        <h1 className="text-white">Welcome to social media graphql</h1>
-        <p className="text-red-500">
-          Não foi possível carregar os dados. Por favor, tente novamente mais
-          tarde.
-        </p> */}
-      </div>
-    );
-  }
+    </div>
+  );
 }
--- frontend/src/lib/actions/user.action.ts
+++ frontend/src/lib/actions/user.action.ts
@@ -1,15 +1,17 @@
+"use server";
+
 import { CREATE_USER, DELETE_USER } from "@/graphql/mutations";
 import client from "../client";
+import { GET_WHOAMI } from "@/graphql/queries";
 import {
   CreateUserParams,
   DeleteUserParams,
   GetUserParams,
 } from "./shared.types";
-import { GET_WHOAMI } from "@/graphql/queries";
-import getToken from "./token.action";
 
 export async function createUser(userData: CreateUserParams) {
   const { clerkId, email, image, username } = userData;
+
   try {
     await client.mutate({
       mutation: CREATE_USER,
@@ -21,13 +23,14 @@ export async function createUser(userData: CreateUserParams) {
       },
     });
   } catch (error) {
-    console.log(error);
+    console.error("Erro ao criar usuário:", error);
     throw error;
   }
 }
 
 export async function deleteUser(deleteUserData: DeleteUserParams) {
   const { clerkId } = deleteUserData;
+
   try {
     await client.mutate({
       mutation: DELETE_USER,
@@ -36,14 +39,14 @@ export async function deleteUser(deleteUserData: DeleteUserParams) {
       },
     });
   } catch (error) {
-    console.log(error);
+    console.error("Erro ao deletar usuário:", error);
     throw error;
   }
 }
 
-export async function getUserData(retryCount = 0): Promise<GetUserParams> {
-  const { token } = await getToken();
-
+export async function getUserData(
+  token: string | null
+): Promise<GetUserParams> {
   try {
     const response = await client.query({
       query: GET_WHOAMI,
@@ -55,12 +58,7 @@ export async function getUserData(retryCount = 0): Promise<GetUserParams> {
     });
     return response.data.whoami;
   } catch (error) {
-    if (retryCount < 3) {
-      console.warn(`Tentativa ${retryCount + 1} falhou. Tentando novamente...`);
-      return await getUserData(retryCount + 1);
-    }
-    throw new Error(
-      "Não foi possível buscar os dados do usuário após 3 tentativas."
-    );
+    console.error("Erro ao buscar dados do usuário:", error);
+    throw new Error("Não foi possível buscar os dados do usuário.");
   }
 }
